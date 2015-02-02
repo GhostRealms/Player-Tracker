0cc3a666c2fcd2ea5f0475bfc9c48a9482087141
River Marmorstein <river@rmarmorstein.com>
2015-02-02 15:03:59 -0500

    using some better object orientation

--- src/main/java/com/rmarmorstein/PlayerTracker/Banlist/Banlist.java
+++ src/main/java/com/rmarmorstein/PlayerTracker/Banlist/Banlist.java
@@ -5,13 +5,9 @@ import com.rmarmorstein.PlayerTracker.PlayerTracker;
 import java.util.UUID;
 
 public abstract class Banlist {
-	PlayerTracker plugin;
-
-	public boolean isBanned( UUID uuid ) {
-		return false;
-	}
 	
-	public boolean isFig() {
-		return false;
-	}
+	PlayerTracker plugin;
+	
+	public abstract boolean isBanned( UUID uuid );
+	
 }
